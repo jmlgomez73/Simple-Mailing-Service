acd04838f054033310ee6ff60d9330bae377babd
Jorge Manuel Lozano Gómez <67438760+jmlgomez73@users.noreply.github.com>
2020-07-03 17:22:55 +0200

    Update Python Email.py

--- Python Email.py
+++ Python Email.py
@@ -1,39 +1,45 @@
 
 # import necessary packages
- 
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 import smtplib
+import sys
  
 # create message object instance
 msg = MIMEMultipart()
 
-x=open("x.py","r")
-xy=x.read()
-
-message = "Put here your message" + xy
- 
 # setup the parameters of the message
+message = "Put here your message" 
 password = "xxx"
 msg['From'] = "xxx@xxx"
 msg['To'] = "xxx@xxx"
 msg['Subject'] = "Put here the subject"
  
-# add in the message body
-msg.attach(MIMEText(message, 'plain'))
+
+try:
+	# add in the message body
+	msg.attach(MIMEText(message, 'plain'))
+
+	#create server
+	server = smtplib.SMTP('smtp.gmail.com: 587')
+	 
+	#identify as an encrypted connection
+	server.ehlo()
+
+	#Adding tls encryptation
+	server.starttls()
+
+	#re-identify as an encrypted connection
+	server.ehlo()
+	 
+	# Login Credentials for sending the mail
+	server.login(msg['From'], password)
  
-#create server
-server = smtplib.SMTP('smtp.gmail.com: 587')
- 
-server.starttls()
- 
-# Login Credentials for sending the mail
-server.login(msg['From'], password)
- 
- 
-# send the message via the server.
-server.sendmail(msg['From'], msg['To'], msg.as_string())
- 
-server.quit()
- 
-print ("successfully sent email to %s:" % (msg['To']))
+	try:
+		# send the message via the server.
+		server.sendmail(msg['From'], msg['To'], msg.as_string())	
+		print ("successfully sent email to %s:" % (msg['To']))	 
+	finally:	       
+		server.quit()
+except:
+	sys.exit( "mail failed; %s" % "CUSTOM_ERROR" )
